abdfc13cf2ffe6868cd484cd9c82c23f88a6ebcf
Wes <wesleyayer92@gmail.com>
2020-04-21 14:12:42 -0400

    adds sentence change to eventlistener

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const noun1 = document.getElementById("noun1");
 const noun2 = document.getElementById("noun2");
 const noun1Input = document.getElementById("noun1Input");
 const noun2Input = document.getElementById("noun2Input");
+const sentence = document.getElementById("sentence");
+const sentenceInput = document.getElementById("sentenceInput");
 
 myButton.addEventListener("click", (e) => {
     e.preventDefault();
     noun1.textContent = noun1Input.value;
     noun2.textContent = noun2Input.value;
+    sentence.textContent = sentenceInput.value;
 });
 
